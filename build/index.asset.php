fe621b41a73f7af5a45fdec4554246d86ace435a
a-newcomer <a.f.newcomer@gmail.com>
2023-06-19 12:03:47 -0700

    updated build files

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'f893ceee439c1855c6ba');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '768a632e3d53d4508ae1');
